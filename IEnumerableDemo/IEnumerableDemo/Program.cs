b1c7b0d39b11b8b27192ffc81448243711206664
giolga <giolga.kumi@gmail.com>
2024-07-24 16:37:11 +0400

    Done

---
+++ IEnumerableDemo/IEnumerableDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> unknownCollection;
+
+            unknownCollection = GetCollection(1);
+            Console.WriteLine("This is a List<int>");
+
+            foreach (int i in unknownCollection)
+            {
+                Console.Write(i + " ");
+            }
+
+            unknownCollection = GetCollection(2);
+
+            Console.WriteLine("\nThis is a Queue<int>");
+            foreach (int i in unknownCollection)
+            {
+                Console.Write(i + " ");
+            }
+
+            unknownCollection = GetCollection(5);
+            Console.WriteLine("\nThis is an array of int");
+
+            foreach (int i in unknownCollection)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadKey();
+        }
+
+        static IEnumerable<int> GetCollection(int option)
+        {
+            List<int> numbersList = new List<int>() { 1, 2, 3, 4, 5 };
+
+            Queue<int> numbersQueue = new Queue<int>();
+            numbersQueue.Enqueue(6);
+            numbersQueue.Enqueue(7);
+            numbersQueue.Enqueue(8);
+            numbersQueue.Enqueue(9);
+            numbersQueue.Enqueue(10);
+
+            if (option == 1)
+            {
+                return numbersList;
+            }
+            else if (option == 2)
+            {
+                return numbersQueue;
+            }
+            else
+            {
+                return new int[] { 11, 12, 13, 14, 15 };
+            }
+
+        }
+    }
+}
---
+++ IEnumerableDemo2/IEnumerableDemo2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersList = new List<int>() { 8, 6, 2 };
+            int[] numberArray = new int[] { 1, 7, 1, 3 };
+
+            CollectionSum(numbersList);
+            CollectionSum(numberArray);
+
+            Console.ReadKey();
+        }
+
+        static void CollectionSum(IEnumerable<int> anyCollection)
+        {
+            int sum = 0;
+
+            foreach (int num in anyCollection)
+            {
+                sum += num;
+            }
+
+            Console.WriteLine("Sum is {0}", sum);
+        }
+    }
+}
